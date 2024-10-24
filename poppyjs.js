4ab742479d1cec7eb153d21fc9df67b840c4cb47
YanfeiDev <152602014+Poppy1234567890@users.noreply.github.com>
2024-10-24 10:28:19 +0100

    Add files via upload

---
+++ poppyjs.js
@@ -0,0 +1,62 @@
+let totalValue = "";
+
+document.getElementById("7").onclick = function() {sevenFunc()};
+document.getElementById("8").onclick = function() {eightFunc()};
+document.getElementById("9").onclick = function() {nineFunc()};
+document.getElementById("4").onclick = function() {fourFunc()};
+document.getElementById("5").onclick = function() {fiveFunc()};
+document.getElementById("6").onclick = function() {sixFunc()};
+document.getElementById("1").onclick = function() {oneFunc()};
+document.getElementById("2").onclick = function() {twoFunc()};
+document.getElementById("3").onclick = function() {threeFunc()};
+
+
+function sevenFunc() {
+    totalValue = totalValue + "7";
+    document.getElementById("output").innerHTML = totalValue;
+}
+
+function sevenFunc() {
+    totalValue = totalValue + "7";
+    console.log(totalValue);
+}
+
+function eightFunc() {
+    totalValue = totalValue + "8";
+    console.log(totalValue);
+}
+
+function nineFunc() {
+    totalValue = totalValue + "9";
+    console.log(totalValue);
+}
+
+function fourFunc() {
+    totalValue = totalValue + "4";
+    console.log(totalValue);
+}
+
+function fiveFunc() {
+    totalValue = totalValue + "5";
+    console.log(totalValue);
+}
+
+function sixFunc() {
+    totalValue = totalValue + "6";
+    console.log(totalValue);
+}
+
+function oneFunc() {
+    totalValue = totalValue + "1";
+    console.log(totalValue);
+}
+
+function twoFunc() {
+    totalValue = totalValue + "2";
+    console.log(totalValue);
+}
+
+function threeFunc() {
+    totalValue = totalValue + "3";
+    console.log(totalValue);
+}
